c19208fd4f0be5adc056787b16b3e95d488dd657
Simon Antonius Lauer <simon.lauer@posteo.de>
2024-01-27 17:29:06 +0100

    Improved query for the start page search.

--- books/books/routes.py
+++ books/books/routes.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, render_template, request
-from data.models import Book
+from data.models import Book, Author
+import sqlalchemy as sa
+from sqlalchemy import or_
+from data.models import db
 
 books = Blueprint("books", __name__, template_folder="templates")
 
@@ -15,8 +18,13 @@ def books_table():
 def search():
     q = request.args.get("q")
     if q:
-        results = Book.query.filter(Book.title.icontains(q)).all()
+        # results = Book.query.filter(Book.title.icontains(q)).all()
+        query = sa.select(Book).join(Author) \
+            .where(or_(Book.title.icontains(q),
+                   Author.fullname.icontains(q)))
+        results = db.session.scalars(query).all()
     else:
         results = []
 
+    print(results)
     return render_template("search_results.html", results=results)
