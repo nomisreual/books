8e5477b2549d0548d4641939b3c4e0f0345e6ae7
Simon Antonius Lauer <simon.lauer@posteo.de>
2024-01-31 10:17:26 +0100

    Added more API endpoints.

--- src/books/api/routes.py
+++ src/books/api/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify
-from data.models import Book
+from data.models import Book, Author
 
 
 api = Blueprint("api", __name__, url_prefix="/api")
@@ -11,7 +11,7 @@ def index():
 
 
 @api.route('/books')
-def get_users():
+def get_books():
     books = Book.query.all()
     books_dictionary = {
         f"{book.id}": {
@@ -21,4 +21,52 @@ def get_users():
         } for book in books
     }
     return jsonify(book=books_dictionary, status=200,
+                   mimetype='application/json')
+
+
+@api.route('/authors')
+def get_authors():
+    authors = Author.query.all()
+    authors_dictionary = {
+        f"{author.id}": {
+            "full_name": author.fullname,
+            "birthdatet": author.birthdate.date(),
+            "books": [book.id for book in author.books]
+        } for author in authors
+    }
+    return jsonify(book=authors_dictionary, status=200,
+                   mimetype='application/json')
+
+
+@api.route('/books/<int:id>')
+def get_book(id: int):
+    book = Book.query.filter_by(id=id).first()
+    if book:
+        book_dictionary = {
+            f"{book.id}": {
+                "title": book.title,
+                "year": book.year,
+                "author": book.author.fullname
+            }
+        }
+        return jsonify(book=book_dictionary, status=200,
+                       mimetype='application/json')
+    return jsonify(message="No result", status=204,
+                   mimetype='application/json')
+
+
+@api.route('/authors/<int:id>')
+def get_author(id: int):
+    author = Author.query.filter_by(id=id).first()
+    if author:
+        author_dictionary = {
+            f"{author.id}": {
+                "full_name": author.fullname,
+                "birthdatet": author.birthdate.date(),
+                "books": [book.id for book in author.books]
+            }
+        }
+        return jsonify(author=author_dictionary, status=200,
+                       mimetype='application/json')
+    return jsonify(message="No result", status=204,
                    mimetype='application/json')
