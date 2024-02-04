246ea39afae7c1fa033a8f33f6209999ea9acfc5
Simon Antonius Lauer <simon.lauer@posteo.de>
2024-02-04 20:34:26 +0100

    Made search actually do in-place replace rather than page reload on page change.

--- src/books/main/routes.py
+++ src/books/main/routes.py
@@ -42,20 +42,17 @@ def author_details(id):
 def search():
     q = request.args.get("q")
     page = request.args.get("page", 1, type=int)
-    if q:
-        query = sa.select(Book).join(Author) \
-            .where(or_(Book.title.icontains(q),
-                       Author.fullname.icontains(q)))
-        results = db.paginate(query, page=page,
-                              per_page=20, error_out=False)
-        next_url = url_for("main.index",
-                           page=results.next_num, q=q) \
-            if results.has_next else None
-        prev_url = url_for("main.index",
-                           page=results.prev_num, q=q) \
-            if results.has_prev else None
-        return render_template("main/_search_results.html",
-                               results=results.items,
-                               next_url=next_url, prev_url=prev_url)
-    else:
-        return render_template("main/_search_results.html", results=None)
+    # if q:
+    query = sa.select(Book).join(Author) \
+        .where(or_(Book.title.icontains(q),
+                   Author.fullname.icontains(q)))
+    results = db.paginate(query, page=page,
+                          per_page=20, error_out=False)
+
+    return render_template("main/_search_results.html",
+                           results=results.items,
+                           q=q, page=page,
+                           next=results.has_next,
+                           prev=results.has_prev)
+    # else:
+    #     return render_template("main/_search_results.html", results=None)
