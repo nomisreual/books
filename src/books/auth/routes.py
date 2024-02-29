2608c10fb87b42853d003ace0b58db6c2bfc11c0
Simon Antonius Lauer <simon.lauer@posteo.de>
2024-02-29 20:54:19 +0100

    Failed login flash message is now properly styled.

--- src/books/auth/routes.py
+++ src/books/auth/routes.py
@@ -19,7 +19,8 @@ def login():
         user = db.session.scalar(
             sa.select(User).where(User.username == form.username.data))
         if user is None or not user.check_password(form.password.data):
-            flash("Invalid username or password.")
+            flash("Invalid username or password.",
+                  "danger")
             return redirect(url_for("auth.login"))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get("next")
