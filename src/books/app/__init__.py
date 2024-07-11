a6247115591ad78f449d26d26090c5ef8f7a4b35
Simon Antonius Lauer <simon.lauer@posteo.de>
2024-07-11 17:13:45 +0200

    chore: disable filter header for APIs
    built-in RestX feature that is not needed, hence it is now disabled.

--- src/books/app/__init__.py
+++ src/books/app/__init__.py
@@ -14,21 +14,22 @@ from extensions import db, migrate, login
 
 
 def create_app(test_config=None):
-
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
 
     # Load environmental variables:
-    DOTENV_BASEDIR = os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                                  os.pardir, "instance"))
+    DOTENV_BASEDIR = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), os.pardir, "instance")
+    )
     dotenv.load_dotenv(os.path.join(DOTENV_BASEDIR, "database.env"))
 
     # Set up Keys:
     app.config.from_mapping(
-        SECRET_KEY=os.getenv("SECRET_KEY") or 'dev',
-        SQLALCHEMY_DATABASE_URI=os.getenv("DEPLOYMENT_DATABASE") or
-        os.getenv("PRODUCTION_DATABASE") or
-        "sqlite:///app.db"
+        SECRET_KEY=os.getenv("SECRET_KEY") or "dev",
+        SQLALCHEMY_DATABASE_URI=os.getenv("DEPLOYMENT_DATABASE")
+        or os.getenv("PRODUCTION_DATABASE")
+        or "sqlite:///app.db",
+        RESTX_MASK_SWAGGER=False,
     )
 
     # Configure custom command "flask seeding"
@@ -39,7 +40,7 @@ def create_app(test_config=None):
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile("config.py", silent=True)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
